894fd688f9abd0bd81f3b80b8547001ef99bb12c
Luc Forget <luc.forget@insa-lyon.fr>
2019-04-08 09:37:59 +0200

    Test tree reorganisation

--- tests/primitives/test_lzoc.cpp
+++
@@ -1,9 +0,0 @@
-#define BOOST_TEST_DYN_LINK
-#define BOOST_TEST_MODULE TestPrimitives
-
-#include <boost/test/unit_test.hpp>
-
-BOOST_AUTO_TEST_CASE(testfailed)
-{
-    BOOST_REQUIRE_MESSAGE(false, "FAILED");
-}
