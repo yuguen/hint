9eda1da545e09668fe3b43cc98f9f89527fc97c3
Luc Forget <luc.forget@insa-lyon.fr>
2019-04-08 11:02:05 +0200

    Shifter sizes

--- include/primitives/shifter.hpp
+++ include/primitives/shifter.hpp
@@ -15,7 +15,7 @@ struct ShifterStageInfo
 	static constexpr bool IsFinalStage = (S==1);
 };
 
-template<int IS, int S, template<unsigned int , bool> class Wrapper>
+template<int IS, int S, template<unsigned int, bool> class Wrapper>
 //IS : Input Size (including sticky bit),
 //S : size of shift counter
 inline Wrapper<IS, false> shifter_sticky_stage(
@@ -27,15 +27,15 @@ inline Wrapper<IS, false> shifter_sticky_stage(
         )
 {
         Wrapper<1<<(S-1), false> padding =  Wrapper<1<<(S-1), false>::generateSequence(fill_bit);
-        Wrapper<1, false> stageNeedsShift = count.get<S-1>();
-        Wrapper<S-1, false> countnext = count.slice<S-2, 0>();
+        Wrapper<1, false> stageNeedsShift = count.template get<S-1>();
+        Wrapper<S-1, false> countnext = count.template slice<S-2, 0>();
 
-        Wrapper<1, false> sticky_in = input.get<0>();
-        Wrapper<IS - (1 << (S-1)), false> low = input.slice<IS - 1 - (1 << (S-1)), 1>();
-        Wrapper<(1 << (S-1)), false> high = input.slice<IS - 1 , IS - (1 << (S-1))>();
+        Wrapper<1, false> sticky_in = input.template get<0>();
+        Wrapper<IS - (1 << (S-1)), false> low = input.template slice<IS - 1 - (1 << (S-1)), 1>();
+        Wrapper<(1 << (S-1)), false> high = input.template slice<IS - 1 , IS - (1 << (S-1))>();
         Wrapper<IS, false> next_stage_input;
-        if (stageNeedsShift.isSet<0>()) {
-                Wrapper<1, false> cur_sticky = Wrapper<1, false>{sticky_in.isSet<0>() or high.or_reduce()};
+        if (stageNeedsShift.template isSet<0>()) {
+                Wrapper<1, false> cur_sticky = Wrapper<1, false>{sticky_in.template isSet<0>() or high.or_reduce()};
                 next_stage_input = Wrapper<1, false>{0}.concatenate(low.concatenate(padding).concatenate(cur_sticky));
         } else {
                 next_stage_input = input;
@@ -53,9 +53,9 @@ inline Wrapper<IS, false> shifter_sticky_stage(
         )
 {
         Wrapper<IS, false> result;
-        if (count.isSet<0>()) {
-                Wrapper<IS - 2, false> low = input.slice<IS - 2, 1>();
-                Wrapper<1, false> sticky_out = input.isSet<0>() or input.isSet<IS-1>();
+        if (count.template isSet<0>()) {
+                Wrapper<IS - 2, false> low = input.template slice<IS - 2, 1>();
+                Wrapper<1, false> sticky_out = input.template isSet<0>() or input.template isSet<IS-1>();
                 result = Wrapper<1, false>{0}.concatenate(low.concatenate(fill_bit).concatenate(sticky_out));
         } else {
                 result = input;
@@ -63,18 +63,18 @@ inline Wrapper<IS, false> shifter_sticky_stage(
         return result;
 }
 
-template<int IS, int S, template<unsigned int , bool> class Wrapper>
-inline Wrapper<IS> shifter_sticky_stage(
-        Wrapper<IS> input,
-        Wrapper<S> count,
-        Wrapper<1> fill_bit = 0,
+template<int IS, int S, bool is_signed, template<unsigned int, bool> class Wrapper>
+inline Wrapper<IS, false> shifter_sticky_stage(
+        Wrapper<IS, is_signed> input,
+        Wrapper<S, false> count,
+        Wrapper<1, false> fill_bit = 0,
         typename std::enable_if<((IS-1) < (1 << (S-1)))>::type * = 0
     )
 {
     constexpr unsigned int nb_null_shift = S - Static_Val<IS-1>::_log2;
     // Incorrect sizes !
-    Wrapper<nb_null_shift, false> shift_weights_will_zero = count.slice<S - 1, S - 1 - nb_null_shift>();
-    Wrapper<S-nb_null_shift, false> next_count = count.slice<S-2-nb_null_shift, 0>();
+    Wrapper<nb_null_shift, false> shift_weights_will_zero = count.template slice<S - 1, S - nb_null_shift>();
+    Wrapper<S-nb_null_shift, false> next_count = count.template slice<S-1-nb_null_shift, 0>();
 
     Wrapper<1, false> stageNeedsShift = shift_weights_will_zero.or_reduce();
     Wrapper<IS, false> ret;
@@ -88,11 +88,11 @@ inline Wrapper<IS> shifter_sticky_stage(
     return ret;
 }
 
-template<int IS, int S, bool isRightShift, template<unsigned int , bool> class Wrapper>
+template<int IS, int S, bool isRightShift, bool is_signed, template<unsigned int , bool> class Wrapper>
 inline Wrapper<IS+1, false> shifter_sticky(
-                Wrapper<IS> input,
-                Wrapper<S> count,
-                Wrapper<1> fill_bit = 0
+                Wrapper<IS, is_signed> input,
+                Wrapper<S, false> count,
+                Wrapper<1, false> fill_bit = 0
     )
 {
         Wrapper<IS, false> fin_input;
@@ -105,8 +105,8 @@ inline Wrapper<IS+1, false> shifter_sticky(
         Wrapper<IS + 1, false> shiftstick = shifter_sticky_stage<IS+1, S, Wrapper>(init_sticky, count, fill_bit);
         Wrapper<IS + 1, false> ret;
         if (isRightShift) {
-            Wrapper<IS, false> high = shiftstick.slice<IS, 1>();
-            Wrapper<1, false> sticky = shiftstick.get<0>();
+            Wrapper<IS, false> high = shiftstick.template slice<IS, 1>();
+            Wrapper<1, false> sticky = shiftstick.template get<0>();
             Wrapper<IS, false> high_reverse = reverse(high);
             ret = high_reverse.concatenate(sticky);
         } else {
