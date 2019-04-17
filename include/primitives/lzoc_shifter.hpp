81864cbf2ddb3bc897ffe6ef4467f1ef0340d7a5
UGUEN Yohann <yohann.uguen@inria.fr>
2019-04-17 10:43:49 +0200

    lzoc clean

--- include/primitives/lzoc_shifter.hpp
+++ include/primitives/lzoc_shifter.hpp
@@ -14,12 +14,6 @@
 
 using namespace std;
 
-// template<int N>
-// constexpr bool isOneBelow2Pow()
-// {
-//     return (((1 << Static_Val<N>::_clog2 ) - 1) == N);
-// }
-
 template<unsigned int N, unsigned int S, template<unsigned int , bool> class Wrapper>
 Wrapper<Static_Val<S>::_storage+N, false> getOneBelow2PowLZOC_shift(
             Wrapper<N, false> const & input,
@@ -28,16 +22,11 @@ Wrapper<Static_Val<S>::_storage+N, false> getOneBelow2PowLZOC_shift(
             typename enable_if<Static_Val<S>::_isOneBelow2Pow and (S>1)>::type* = 0
         )
 {
-    // cerr << "Ge 2 S: " << S << endl;
-    // cerr << "input: " << to_string(input) << endl; 
+
     constexpr int upper_half = (1 <<Static_Val<S>::_flog2);
-    // cerr << "upper_half: " << upper_half << endl; 
 
     auto upper = input.template slice<N-1, N-upper_half>();
     auto lower = input.template slice<N-upper_half-1, 0>();
-
-    // auto comp_seq = Wrapper<upper_half, false>::generateSequence(leading);
-    // auto comp = (upper == comp_seq);
 
 
     auto and_red = upper.and_reduction();
@@ -45,21 +34,12 @@ Wrapper<Static_Val<S>::_storage+N, false> getOneBelow2PowLZOC_shift(
     auto comp = Wrapper<1, false>::mux(leading, and_red, Wrapper<1, false>{not(or_red).template isSet<0>()});
 
     auto padding = Wrapper<upper_half, false>::generateSequence(fill_bit);
-    // if(fill_bit.template isSet<0>()){
-    //     padding = invert(Wrapper<upper_half, false>{0}); 
-    // }
-    // else{
-    //     padding = Wrapper<upper_half, false>{0}; 
-    // }
-
-    // auto padding = Wrapper<upper_half, false>::generateSequence(fill_bit);
 
     auto next_stage_input = Wrapper<N, false>::mux(
                 comp,
                 lower.concatenate(padding),
                 input
             );
-    // cerr << "next stage input: " << to_string(next_stage_input) << endl; 
 
     auto ret = comp.concatenate(getOneBelow2PowLZOC_shift<N, upper_half-1>(next_stage_input, leading, fill_bit));
     return ret;
@@ -101,96 +81,6 @@ Wrapper<Static_Val<S>::_storage +N, false> getOneBelow2PowLZOC_shift(
     return ret;
 }
 
-/*
-template<unsigned int N, unsigned int S, template<unsigned int , bool> class Wrapper>
-Wrapper<Static_Val<S>::_storage + N, false> LZOC_shift_stage(
-            Wrapper<N, false> const & input,
-            Wrapper<1, false> const & leading,
-            Wrapper<1, false> const & fill_bit = 0,
-            typename enable_if<(Static_Val<S>::_isOneBelow2Pow and (S > 1)) or (S==1)>::type* = 0
-        )
-{
-    // cerr << "Call lzoc shift on size " << N << endl;
-    return getOneBelow2PowLZOC_shift<N, S>(input, leading, fill_bit);
-}
-
-template<unsigned int N, unsigned int S, template<unsigned int , bool> class Wrapper>
-Wrapper<Static_Val<S>::_storage + N, false> LZOC_shift_stage (
-        Wrapper<N, false> const & input,
-    Wrapper<1, false> const & leading,
-    Wrapper<1, false> const & fill_bit = 0,
-    typename enable_if<Static_Val<S>::_is2Pow and (S > 1)>::type* = 0
-)
-{
-    auto upper = input.template slice<N-1, N-S+1>();
-    auto lzoc_shift_up = getOneBelow2PowLZOC_shift<N, S-1>(input, leading, fill_bit);
-    auto lzoc_up = lzoc_shift_up.template slice<Static_Val<S>::_clog2+N-1, N>();
-    auto shift_up = lzoc_shift_up.template slice<N-1, 0>();
-
-    auto is_full_one = lzoc_shift_up.and_reduction();
-    auto last_bit_is_leading = input.template get<N-1>() == leading;
-    Wrapper<Static_Val<S>::_clog2, false> zeros{0};
-    auto onezeroseq = Wrapper<1, false>{1}.concatenate(zeros);
-    auto uncomplete = Wrapper<1, false>{0}.concatenate(lzoc_up);
-
-    auto cond = is_full_one.bitwise_and(last_bit_is_leading);
-    auto lzoc = Wrapper<Static_Val<S>::_storage, false>::mux(cond, onezeroseq, uncomplete);
-    auto shift = Wrapper<N, false>::mux(
-                cond,
-                shift_up.template slice<N-2, 0>().concatenate(fill_bit),
-                shift_up);
-
-    return lzoc.concatenate(shift);
-}
-
-template<unsigned int N, unsigned int S, template<unsigned int , bool> class Wrapper>
-Wrapper<Static_Val<S>::_storage + N, false> LZOC_shift_stage (
-        Wrapper<N, false> const & input,
-        Wrapper<1, false> const & leading,
-        Wrapper<1, false> const & fill_bit = 0,
-        typename enable_if<(not Static_Val<S>::_is2Pow) and (not Static_Val<S>::_isOneBelow2Pow) and (S > 1)>::type* = 0
-)
-{
-    constexpr int upper_size = (1 << (Static_Val<S>::_flog2)) - 1;
-    auto lzoc_shift_up = getOneBelow2PowLZOC_shift<N, upper_size>(input, leading, fill_bit);
-
-    auto lzoc_up = lzoc_shift_up.template slice<N + Static_Val<upper_size>::_clog2 -1,N>();
-    auto shift_up = lzoc_shift_up.template slice<N-1,0>();
-    auto is_full_one = lzoc_up.and_reduction();
-    auto last_bit_is_leading = lzoc_shift_up.template get<N-1>() == leading;
-    auto msb = is_full_one.bitwise_and(last_bit_is_leading);
-
-    //If all was zero, we only need to count on the lowest bits
-    constexpr unsigned int low_shift_size = N - (upper_size+1);
-    auto low_lzoc_input = shift_up.template slice<N-2, N-low_shift_size-1>();
-    constexpr unsigned int count_size_low = S-(upper_size+1);
-    constexpr unsigned int low_lzoc_size = Static_Val<count_size_low>::_storage;
-    constexpr unsigned int low_lzoc_total_size = low_lzoc_size + low_shift_size;
-
-    Wrapper<low_lzoc_total_size, false> lzoc_shift_low = LZOC_shift_stage<low_shift_size, count_size_low>(
-                low_lzoc_input,
-                leading,
-                fill_bit
-            );
-    auto lzoc_low = lzoc_shift_low.template slice<low_lzoc_total_size-1, low_lzoc_total_size-low_lzoc_size>();
-    auto shift_low = lzoc_shift_low.template slice<low_shift_size - 1, 0>();
-    auto ext_lowcount = lzoc_low.template leftpad<Static_Val<upper_size>::_clog2>();
-    auto padding = Wrapper<upper_size+1, false>::generateSequence(fill_bit);
-
-    auto lsb = Wrapper<Static_Val<upper_size>::_clog2, false>::mux(
-                msb,
-                ext_lowcount,
-                lzoc_up
-            );
-
-    auto lzoc = msb.concatenate(lsb);
-    auto shift = Wrapper<N, false>::mux(
-        msb,
-        shift_low.concatenate(padding),
-        shift_up
-        );
-    return lzoc.concatenate(shift);
-}*/
 
 template<unsigned int N, unsigned int S, template<unsigned int , bool> class Wrapper>
 Wrapper<Static_Val<S>::_storage + N, false> LZOC_shift_impl(
