f222434388615e2d2a05577a12b820cebc13780d
UGUEN Yohann <yohann.uguen@inria.fr>
2019-04-17 17:01:26 +0200

    reductions with inline

--- include/primitives/reductions.hpp
+++ include/primitives/reductions.hpp
@@ -3,11 +3,12 @@
 
 
 template<unsigned int N, template<unsigned int, bool> class Wrapper>
-Wrapper<1, false> or_reduction(
+inline Wrapper<1, false> or_reduction(
 	Wrapper<N, false> const input,
     typename enable_if<(N>1)>::type* = 0
 	)
 {
+	#pragma HLS INLINE
 	constexpr unsigned int up_size = (N>>1);
 	constexpr unsigned int low_size = N-up_size;
 	auto high = input.template slice<N-1, N-up_size>();
@@ -20,21 +21,23 @@ Wrapper<1, false> or_reduction(
 
 
 template<unsigned int N, template<unsigned int, bool> class Wrapper>
-Wrapper<1, false> or_reduction(
+inline Wrapper<1, false> or_reduction(
 	Wrapper<N, false> const input,
     typename enable_if<(N==1)>::type* = 0
 	)
 {
+	#pragma HLS INLINE
 	return input;
 }
 
 
 
 template<unsigned int N, bool is_signed, template<unsigned int, bool> class Wrapper>
-Wrapper<1, false> or_reduction(
+inline Wrapper<1, false> or_reduction(
 	Wrapper<N, is_signed> const input 
 	)
 {
+	#pragma HLS INLINE
 	return or_reduction_impl(input.as_unsigned());
 }
 
