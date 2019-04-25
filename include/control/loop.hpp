ed300203693eb942629b16b08e4d28b833a0d0c1
Luc Forget <luc.forget@insa-lyon.fr>
2019-04-25 14:33:17 +0200

    Exporting target

--- include/control/loop.hpp
+++ include/control/loop.hpp
@@ -1,11 +1,11 @@
 #ifndef LOOP_HPP
 #define LOOP_HPP
 
-#include <cstddef>
-#include <functional>
-#include <type_traits>
+//#include <cstddef>
+//#include <functional>
+//#include <type_traits>
 
-using namespace std;
+//using namespace std;
 /***
  *
  * Something to do here when every backend support C++17
