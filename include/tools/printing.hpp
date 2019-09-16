a47b86c5e8cd76f563a860e70a3f796e773e1949
UGUEN Yohann <yohann.uguen@inria.fr>
2019-09-16 11:28:15 +0200

    using namespace in tools/printing'

--- include/tools/printing.hpp
+++ include/tools/printing.hpp
@@ -7,32 +7,33 @@
 
 using namespace std;
 #include "hint.hpp"
+namespace hint {
+    template <typename val, unsigned int W, bool is_signed, template<unsigned int, bool> class wrapper>
+    struct printer{
+        inline constexpr void operator()(stringstream & s, wrapper<W, is_signed> const & signal)
+        {
+            s << (signal.template isSet<val::value>() ? '1' : '0');
+            printer<integral_constant<unsigned int, val::value-1>, W, is_signed, wrapper>{}(s, signal);
+        }
+    };
 
-template <typename val, unsigned int W, bool is_signed, template<unsigned int, bool> class wrapper>
-struct printer{
-    inline constexpr void operator()(stringstream & s, wrapper<W, is_signed> const & signal)
+    template <unsigned int W, bool is_signed, template<unsigned int, bool> class wrapper>
+    struct printer<integral_constant<unsigned int, 0>, W, is_signed, wrapper>
     {
-        s << (signal.template isSet<val::value>() ? '1' : '0');
-        printer<integral_constant<unsigned int, val::value-1>, W, is_signed, wrapper>{}(s, signal);
-    }
-};
+        inline constexpr void operator()(stringstream& s, wrapper<W, is_signed> const & signal)
+        {
+            s << (signal.template isSet<0>() ? '1' : '0');
+        }
+    };
 
-template <unsigned int W, bool is_signed, template<unsigned int, bool> class wrapper>
-struct printer<integral_constant<unsigned int, 0>, W, is_signed, wrapper>
-{
-    inline constexpr void operator()(stringstream& s, wrapper<W, is_signed> const & signal)
+
+    template <unsigned int W, bool is_signed, template<unsigned int, bool> class wrapper>
+    string to_string(wrapper<W, is_signed> const & signal)
     {
-        s << (signal.template isSet<0>() ? '1' : '0');
+        stringstream s{};
+        printer<integral_constant<unsigned int, W-1>, W, is_signed, wrapper>{}(s, signal);
+        return s.str();
     }
-};
-
-
-template <unsigned int W, bool is_signed, template<unsigned int, bool> class wrapper>
-string to_string(wrapper<W, is_signed> const & signal)
-{
-    stringstream s{};
-    printer<integral_constant<unsigned int, W-1>, W, is_signed, wrapper>{}(s, signal);
-    return s.str();
 }
 
 #endif // PRINTING_HPP
