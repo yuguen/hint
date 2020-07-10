3a7e7709787da89fa415c358d049d4fc145330b8
Luc Forget <luc.forget@insa-lyon.fr>
2020-07-10 16:49:58 +0200

    Backward

--- include/primitives/backwards.hpp
+++ include/primitives/backwards.hpp
@@ -1,15 +1,27 @@
 #ifndef BACKWARDS_HPP
 #define BACKWARDS_HPP
 
+#include <type_traits>
+
 namespace hint {
 	//In case we need both the variable and its reversed value
 	//(vivado reverse change the bit order of the value on which its called)
 	template <unsigned int N, bool is_signed, template<unsigned int , bool> class Wrapper>
 	Wrapper<N, false> backwards(
-			Wrapper<N, is_signed> input
+			Wrapper<N, is_signed> input,
+			typename std::enable_if<(N>1)>::type* = 0
 		)
 	{
-		return input.backwards();
+		return input.template get<0>().concatenate(backwards(input.template slice<N-1, 1>()));
+	}
+
+	template <unsigned int N, bool is_signed, template<unsigned int , bool> class Wrapper>
+	Wrapper<N, false> backwards(
+			Wrapper<N, is_signed> input,
+			typename std::enable_if<(N<=1)>::type* = 0
+		)
+	{
+		return input;
 	}
 }
 #endif // BACKWARDS_HPP
