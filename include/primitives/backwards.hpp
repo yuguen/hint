3d210e9bff7c3a733819eebf17e2dc942ce453e3
Luc Forget <luc.forget@insa-lyon.fr>
2020-08-18 07:56:12 +0200

    Backward

--- include/primitives/backwards.hpp
+++ include/primitives/backwards.hpp
@@ -3,37 +3,25 @@
 
 #include <type_traits>
 
-using std::enable_if;
-
 namespace hint {
-
-    template<unsigned int idx, unsigned int N, template<unsigned int , bool> class Wrapper>
-    inline void do_backward(Wrapper<N, false> const & input,
-                       Wrapper<N, false> & output,
-                       typename enable_if<(idx > 0)>::type* = 0)
-    {
-        output.template affect_bit<idx, N-1-idx>(input);
-        do_backward<idx-1>(input, output);
-    }
-
-    template<unsigned int idx, unsigned int N, template<unsigned int , bool> class Wrapper>
-    inline void do_backward(Wrapper<N, false> const & input,
-                       Wrapper<N, false> & output,
-                       typename enable_if<(idx == 0)>::type* = 0)
-    {
-        output.template affect_bit<idx, N-1-idx>(input);
-    }
-
+	//In case we need both the variable and its reversed value
+	//(vivado reverse change the bit order of the value on which its called)
+	template <unsigned int N, bool is_signed, template<unsigned int , bool> class Wrapper>
+	inline Wrapper<N, false> backwards(
+			Wrapper<N, is_signed> input,
+			typename std::enable_if<(N>1)>::type* = 0
+		)
+	{
+		return input.template get<0>().concatenate(backwards(input.template slice<N-1, 1>()));
+	}
 
 	template <unsigned int N, bool is_signed, template<unsigned int , bool> class Wrapper>
-    inline Wrapper<N, false> backwards(
-            Wrapper<N, is_signed> const input
+	inline Wrapper<N, false> backwards(
+			Wrapper<N, is_signed> input,
+			typename std::enable_if<(N<=1)>::type* = 0
 		)
-    {
-       auto usi = input.as_unsigned();
-       Wrapper<N, false> result;
-       do_backward<N-1>(usi, result);
-       return result;
-    }
+	{
+		return input;
+	}
 }
 #endif // BACKWARDS_HPP
