800aa28c0da5ca68d9d63435e4e0ff126b16e1b8
Luc Forget <luc.forget@insa-lyon.fr>
2020-11-18 09:43:50 +0100

    Static maths function take their argument from template parameter instead of standard argument

--- include/tools/static_math.hpp
+++ include/tools/static_math.hpp
@@ -8,37 +8,40 @@ namespace hint {
 			static constexpr int val = (num % denom) ? (num / denom) + 1 : num / denom;
 	};
 
-	constexpr unsigned int ceilLog2(unsigned int N, uint8_t remains = 0)
+	template<unsigned int N, uint8_t remains=0>
+	constexpr inline unsigned int ceilLog2()
 	{
-			return (N <= 1) ? remains : 1 + ceilLog2(N>>1, remains | (N%2));
+		return (N <= 1) ? remains : 1 + ceilLog2<(N>>1), remains | (N%2)>();
 	}
 
-	constexpr unsigned int ceil2Power(unsigned int N)
+	template<unsigned int N>
+	constexpr inline unsigned int ceil2Power()
 	{
-			return 1 << ceilLog2(N);
+		return 1 << ceilLog2<N>();
 	}
 
-	constexpr unsigned int log2(unsigned int N)
+	template<unsigned int N>
+	constexpr inline unsigned int log2()
 	{
-			 return ((N<2) ? 1 : 1+log2(N>>1));
+		return ((N<2) ? 1 : 1+log2<(N>>1)>());
 	}
 
-	constexpr unsigned int r2pow(unsigned int N)
+	template<unsigned int N>
+	constexpr inline unsigned int r2pow()
 	{
-			 return 1 << log2(N);
+		return 1 << log2<N>();
 	}
 
-	constexpr unsigned int floorLog2(unsigned int N)
+	template <unsigned int N>
+	constexpr inline unsigned int floorLog2()
 	{
-		return (N<= 1) ? 0 : 1 + floorLog2(N>>1);
+		return (N<= 1) ? 0 : 1 + floorLog2<(N>>1)>();
 	}
-
-
 
 	template<unsigned int N>
 	constexpr bool is2Pow()
 	{
-		return (1 << ceilLog2(N)) == N;
+		return (1 << ceilLog2<N>()) == N;
 	}
 
 
@@ -51,16 +54,16 @@ namespace hint {
 	template<unsigned int N>
 	class Static_Val
 	{
-			public:
-					static constexpr unsigned int _rlog2 = log2(N);
-					static constexpr unsigned int _r2pow = r2pow(N);
-					static constexpr unsigned int _log2 = ceilLog2(N);
-					static constexpr unsigned int _2pow = ceil2Power(N);
-					static constexpr unsigned int _flog2 = floorLog2(N);
-					static constexpr unsigned int _clog2 = ceilLog2(N);
-					static constexpr unsigned int _storage = ceilLog2(N+1);
-					static constexpr bool _is2Pow = is2Pow<N>();
-					static constexpr bool _isOneBelow2Pow = isOneBelow2Pow<N>();
+		public:
+			static constexpr unsigned int _rlog2 = log2<N>();
+			static constexpr unsigned int _r2pow = r2pow<N>();
+			static constexpr unsigned int _log2 = ceilLog2<N>();
+			static constexpr unsigned int _2pow = ceil2Power<N>();
+			static constexpr unsigned int _flog2 = floorLog2<N>();
+			static constexpr unsigned int _clog2 = ceilLog2<N>();
+			static constexpr unsigned int _storage = ceilLog2<N+1>();
+			static constexpr bool _is2Pow = is2Pow<N>();
+			static constexpr bool _isOneBelow2Pow = isOneBelow2Pow<N>();
 	};
 
 	constexpr unsigned int prod_contrib(unsigned int N)
