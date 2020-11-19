258cc753455f607f76dadb6af89ac7a4a0b8a1fa
Luc Forget <luc.forget@insa-lyon.fr>
2020-11-19 11:24:52 +0100

    Use built-in backward

--- include/primitives/backwards.hpp
+++ include/primitives/backwards.hpp
@@ -8,20 +8,21 @@ namespace hint {
 	//(vivado reverse change the bit order of the value on which its called)
 	template <unsigned int N, bool is_signed, template<unsigned int , bool> class Wrapper>
 	inline Wrapper<N, false> backwards(
-			Wrapper<N, is_signed> input,
-			typename std::enable_if<(N>1)>::type* = 0
+			Wrapper<N, is_signed> input/*,
+			typename std::enable_if<(N>1)>::type* = 0*/
 		)
 	{
-		return input.template get<0>().concatenate(backwards(input.template slice<N-1, 1>()));
+		return input.backwards();
+		//return input.template get<0>().concatenate(backwards(input.template slice<N-1, 1>()));
 	}
 
-	template <unsigned int N, bool is_signed, template<unsigned int , bool> class Wrapper>
+	/*template <unsigned int N, bool is_signed, template<unsigned int , bool> class Wrapper>
 	inline Wrapper<N, false> backwards(
 			Wrapper<N, is_signed> input,
 			typename std::enable_if<(N<=1)>::type* = 0
 		)
 	{
 		return input;
-	}
+	}*/
 }
 #endif // BACKWARDS_HPP
